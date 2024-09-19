4fb24e8a3485600e3b599235f9672f18192498a7
ryanabest <ryanabest@gmail.com>
2024-09-19 12:35:01 -0400

    update line chart with circles

--- src/js/utils/teams.js
+++ src/js/utils/teams.js
@@ -1,10 +1,10 @@
 export const colors = {
   "Dan": "#e41a1c",
-  "David": "#377eb8",
+  "David": "#f781bf",
   "Eamonn": "#4daf4a",
   "John": "#984ea3",
   "Ray": "#ff7f00",
   "Ryan": "#dede40",
   "Sean": "#a65628",
-  "Skye": "#f781bf",
+  "Skye": "#377eb8",
 }
--- src/lib/LineChart.svelte
+++ src/lib/LineChart.svelte
@@ -2,17 +2,20 @@
   export let data;
   import d3 from "../js/d3";
   import { colors } from "../js/utils/teams";
+  import { slugify } from "../js/utils/format";
 
   let width; // this will be populated with the width of our chart containers via svelte's bind:clientWidth={w}
   let height; // this will be populated with the height of our chart containers via svelte's bind:clientHeight={w}
-  const margin = { top: 30, bottom: 20, left: 30, right: 150 };
+  const margin = { top: 30, bottom: 20, left: 30, right: 100 };
 
   const startDate = data.teams[0].pointsByDate[0].date;
   const endDate = data.teams[0].pointsByDate[data.teams[0].pointsByDate.length - 1].date;
 
   const maxPts = Math.max(...data.teams.map(d => d.points));
 
-  // scales and line
+  const strokeWidth = 4;
+
+  // ~~ LINE SCALES ~~ //
   $: xScale = d3.scaleUtc([new Date(startDate), new Date(endDate)],[margin.left, width - margin.right - margin.left]);
   $: yScale = d3.scaleLinear([0, maxPts],[height - margin.bottom - margin.top, margin.top]);
   $: line = d3.line()
@@ -56,32 +59,86 @@
       zeroTick.classed('zero', true);
     });
 
-  const teamsSorted = data.teams.sort((a, b) => a.points - b.points);
+  // ~~ CIRCLE PARAMS ~~ //
+  const radius = 25;
+  const swoopyWidth = 25;
+
+  // ~~ SWOOPY DATA ~~ //
+  $: _genSwoopyData = (team, i) => {
+    const swoopyData = [];
+    const d = team.pointsByDate[team.pointsByDate.length - 1];
+    if (!xScale || !yScale) return [];
+    const leftX = xScale(new Date(d.date))
+    const leftY = yScale(d.points);
+    const rightX = width - margin.right - margin.left + swoopyWidth
+    const rightY = radius + (((radius + (strokeWidth / 2)) * 2) * (data.teams.length - i - 1));
+    const xDiff = rightX - leftX;
+    const yDiff = rightY - leftY;
+
+    for (let i = -6; i < 6; i++) {
+      const sigY = leftY + (sigmoid(i) * yDiff);
+      const sigX = leftX + (((i + 6) / 11) * xDiff);
+      swoopyData.push({ x: sigX, y: sigY });
+    }
+
+    function sigmoid (t) {
+      return 1 / (1 + Math.pow(Math.E, -t));
+    }
+    
+    return swoopyData;
+  }
+
+  $: swoopyLine = d3.line()
+    .x(d => d.x)
+    .y(d => d.y);
+
+  $: teamsSorted = data.teams
+    .sort((a, b) => a.points - b.points)
+    .map((team, i) => {
+      const swoopyData = _genSwoopyData(team, i);
+      return { ...team, swoopyData }
+    });
 </script>
 
 <div class="chart-cont" bind:clientWidth={width} bind:clientHeight={height}>
   <svg>
     <g class="chart" transform="translate({margin.left},{margin.top})">
       <g class="axis-g" bind:this={gy}></g>
+      <g class="swoop-g">
+        {#each teamsSorted as d}
+          <path d={swoopyLine(d.swoopyData)} stroke={colors[d.team]} stroke-width=1 stroke-linecap="round" stroke-dasharray="0 2"></path>
+        {/each}
+      </g>
       <g class="line-g">
         {#each teamsSorted as d}
           <g class="team" data-team={d.team}>
-            <path stroke="#ffffff" stroke-width="6" d={line(d.pointsByDate)}></path>
-            <path stroke={colors[d.team]} stroke-width="4" d={line(d.pointsByDate)}></path>
+            <path stroke="#ffffff" stroke-width={strokeWidth * 1.5} d={line(d.pointsByDate)}></path>
+            <path stroke={colors[d.team]} stroke-width={strokeWidth} d={line(d.pointsByDate)}></path>
           </g>
         {/each}
       </g>
-      <g class="swoop-g">
-
-      </g>
       <g class="circle-g">
-        {#each teamsSorted as d}
+        {#each teamsSorted as d, i}
           <circle
-            id='circle-{d.team}'
+            id='circle-{slugify(d.team)}'
+            r={radius}
+            cy={radius + (((radius + (strokeWidth / 2)) * 2) * (teamsSorted.length - i - 1))}
+            cx={width - margin.right - margin.left + radius + swoopyWidth}
+            stroke={colors[d.team]}
+            stroke-width={strokeWidth}
+            fill="url(#pattern-{slugify(d.team)})"
           />
         {/each}
+        <!-- cy={(radius * 2) * (teamsSorted.length - i - 1)} -->
       </g>
     </g>
+    <defs>
+      {#each teamsSorted as d}
+        <pattern id="pattern-{slugify(d.team)}" height=100% width=100% patternContentUnits="objectBoundingBox">
+          <image x="0" y="0" height=1 width=1 preserveAspectRatio="xMidYMid slice" xlink:href="public/{slugify(d.team)}.png" />
+        </pattern>
+      {/each}
+    </defs>
   </svg>
 </div>
 
