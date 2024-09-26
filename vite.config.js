4e5ed53493d6cf0040a82c53c17454acd55aa0e4
ryanabest <ryanabest@gmail.com>
2024-09-26 17:24:43 -0400

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/2024-ta-epl-fantasy/'
+  base: '/epl-fantasy/'
 })
