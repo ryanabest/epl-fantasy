929a7e691025899f1e321effad29eccc1735b396
ryanabest <ryanabest@gmail.com>
2025-08-04 15:07:51 -0400

    try building new html files in vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/epl-fantasy/'
+  base: '/epl-fantasy/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        2024: resolve(__dirname, '2024.html'),
+        2025: resolve(__dirname, '2025.html'),
+      },
+    },
+  },
 })
