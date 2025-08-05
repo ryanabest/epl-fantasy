948e9de7110290d56e42d81da1c676edcc46d371
ryanabest <ryanabest@gmail.com>
2025-08-05 17:29:21 -0400

    add kelsea

--- src/js/utils/teams.js
+++ src/js/utils/teams.js
@@ -10,16 +10,17 @@ export const colors = {
     "Skye": "#377eb8",
   },
   2025: {
+    "Junghye": "#C73C0E",
     "Elliot": "#FA7067",
-    "David": "#f781bf",
-    "Eamonn": "#4daf4a",
-    "John": "#B1A2E9",
     "Thomas": "#ff7f00",
     "Ryan": "#dede40",
-    "Dan": "#9ACCFF",
-    "Skye": "#377eb8",
     "Yuriko": "#136060",
-    "Junghye": "#C73C0E",
+    "Eamonn": "#088449",
+    "David": "#f781bf",
+    "John": "#B1A2E9",
+    "Dan": "#6454A2",
+    "Skye": "#377eb8",
+    "Kelsea": "#9ACCFF",
   }
 
 }
