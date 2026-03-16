da4d39fffa0c0445bd05826989da191ca19e082d
ryanabest <ryanabest@gmail.com>
2026-03-16 16:46:09 -0400

    update player profiles

--- sportradar/compiler.cjs
+++ sportradar/compiler.cjs
@@ -73,12 +73,14 @@ class Compiler {
         defender: "DEF",
         goalkeeper: "GK"
       }[p.player.type];
-      let jersey = p.player.jersey_number;
-      if (!jersey) {
-        const premTeam = p.competitors.find(t => t.country === 'England' && t.name !== 'England');
-        const role = p.roles.find(r => r.competitor.id === premTeam?.id);
-        jersey = role?.jersey_number;
-      }
+
+      let jersey;
+
+      const premTeam = p.competitors.find(t => t.country === 'England' && t.name !== 'England');
+      const role = p.roles?.find(r => r.competitor?.id === premTeam?.id);
+      jersey = role?.jersey_number;
+      if (!jersey) jersey = p.player.jersey_number;
+
       const playerTeam = p.competitors?.find(d => {
         return (
           (d.name !== d.country) &&
